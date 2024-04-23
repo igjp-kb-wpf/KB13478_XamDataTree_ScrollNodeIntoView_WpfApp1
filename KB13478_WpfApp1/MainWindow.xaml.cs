51f9d0fd35844e78349f86ca0fee01c0b57c25f2
norikois <33947244+norikois@users.noreply.github.com>
2024-04-23 15:38:46 +0900

    初回コミット

---
+++ KB13478_WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Infragistics.Controls.Menus;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KB13478_WpfApp1;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void button1_Click(object sender, RoutedEventArgs e)
+    {
+        var vm = (MainWindowViewModel)DataContext;
+        var purchase = vm.CustomersAndPurchases.Last().Purchases.Last();
+        var node = xamDataTree1.GetNodesFromItems([purchase], true).FirstOrDefault();
+        if(node != null)
+        {
+            xamDataTree1.ScrollNodeIntoView(node);
+            xamDataTree1.SelectedDataItems = [purchase];
+            node.IsActive = true;
+        }
+    }
+}
---
+++ KB13478_WpfApp1/Model/Purchase.cs
@@ -0,0 +1,28 @@
+﻿using KB13478_WpfApp1.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KB13478_WpfApp1.Model;
+internal class Purchase : ObservableObject
+{
+    private String _orderId = "";
+    public String OrderID
+    {
+        get { return _orderId; }
+        set { _orderId = value; OnPropertyChanged(); }
+    }
+
+    private String _productName = "";
+    public String ProductName
+    {
+        get { return _productName; }
+        set { _productName = value; OnPropertyChanged(); }
+    }
+
+    public Purchase()
+    {
+    }
+}
